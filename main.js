1cecb2ea32197ac79795fd86813172d2699132f7
Murad <agariomadman@gmail.com>
2019-05-31 23:28:09 -0500

    XP rehab

--- main.js
+++ main.js
@@ -12,10 +12,6 @@ var figlet      = require('figlet');
 // Fancy looking CLI
 var inquirer    = require('inquirer');
 
-// Cool spinner to signify loading
-var CLI         = require('clui');
-var Spinner     = CLI.Spinner;
-
 // In-case i need to use JSON files
 var fs          = require('fs');
 
@@ -29,8 +25,9 @@ var monsters = require('./monsters.js')
 // player XP
 
 var xpee = require('./xp.js')
+var xp = 2.74
 
-if(xpee.xpe.xp < 2.74){
+if(xpee === ""){
   fs.writeFile(__dirname + "/xp.js", "var xpe = new function(){\n  this.xp = 2.74\n}\n\nexports.xpe = xpe", function (err) {
       if (err) {
           return console.log("Error writing file: " + err);
@@ -38,12 +35,27 @@ if(xpee.xpe.xp < 2.74){
   });
 }
 
-function gainXP(xp){
-      fs.writeFile(__dirname + "/xp.js", "var xpe = new function(){\n  this.xp = " + xp + "\n}\n\nexports.xpe = xpe", function (err) {
+var player = new function(){
+  this.username = [],
+  this.savedusername = username3.username2.username,
+  this.health = Math.round(xp/(750 / 100) * xp * 100),
+  this.level =  Math.round(xp/(750 / 100) * xp * 100) / 100,
+  this.attack = this.level
+}
+
+if(xpee.xpe.xp === 2.74){
+}else{
+    console.log('yep')
+    xp = xpee.xpe.xp
+}
+
+function gainXP(xpp){
+      fs.writeFile(__dirname + "/xp.js", "var xpe = new function(){\n  this.xp = " + xpp + "\n}\n\nexports.xpe = xpe", function (err) {
           if (err) {
               return console.log("Error writing file: " + err);
           }
       });
+    xp = xpp
 }
 
 //--- Start of code ---
@@ -51,27 +63,6 @@ function gainXP(xp){
 var stage = []
 
 var useSave = []
-
-var player = new function(){
-  this.username = [],
-  this.savedusername = username3.username2.username
-  this.xp = xpee.xpe.xp,
-  this.health = Math.round(this.xp/(750 / 100) * this.xp * 100),
-  this.level =  Math.round(this.xp/(750 / 100) * this.xp * 100) / 100
-  this.attack = this.level
-}
-
-var monsterGUI = [
-  {
-    type: 'list',
-    name: 'monsterGUI',
-    message: 'What would you like to do?',
-    choices: ['Fight! (' + player.attack + ' damage per turn)', 'Run away', ' or Dodge'],
-    filter: function (val) {
-      return val.toLowerCase();
-    }
-  }
-];
 
 function usernamefr(){
     if(username3.username2.username === "#^&^@$^@#&^$@&*^@!^$!&^$*!&^%&!#^$&!#^%#&&&#&#&#&#&#$!^&#*!^"){
@@ -88,7 +79,7 @@ var usesavee =
     {
       type: 'confirm',
       name: 'usesavedd',
-      message: `Welcome back ` + username3.username2.username + ', would you like to change your name?',
+      message: `Welcome back ` + username3.username2.username + ', would you like to change your name and reset your XP?',
       default: true
     }
 
@@ -123,16 +114,28 @@ function name(){
     }
 }
 
+var monsterGUI = [
+  {
+    type: 'list',
+    name: 'monsterGUI',
+    message: 'What would you like to do?',
+    choices: ['Fight! (' + player.attack + ' damage per turn)', 'Run away', 'Dodge'],
+    filter: function (val) {
+      return val.toLowerCase();
+    }
+  }
+];
+
+
 function calc(pDam, mDam, pHealth, mHealth){
 
   if(mHealth - pDam < pHealth - mDam){
 
-    console.log(
-    'does it even run')
   if(mHealth - pDam < pHealth - mDam === true){
 
     console.log(chalk.cyan('Congratz, you won and gained ' + Math.round(player.level / 10 * 100) / 100 + ' xp!'))
-    gainXP(player.xp + player.level / 10)
+    gainXP(xp + player.level / 10)
+    console.log(xp)
   } else if(!mHealth - pDam < pHealth - mDam){
     return console.log(chalk.red('I\'m sorry but you have unfortunatley lost.\n You will now start from the beginning'))
     setTimeout(function(){
@@ -147,29 +150,6 @@ function calc(pDam, mDam, pHealth, mHealth){
     }, 5000);
   }
 }
-
-console.log(player.health - monsters.mDummy.attack < monsters.mDummy.health - player.attack)
-
-function mDummy(){
-  console.log(chalk.red('Uh oh, your first monster has appeared! He appears to be a dummy! \n He appears to do no damage and have 1 health'))
-  inquirer.prompt(monsterGUI).then(function (answers) {
-    var answer = answers["monsterGUI"]
-    console.log(chalk.magenta('\nYou have chosen: ' + answer));
-      if(answer === 'fight! (' + player.attack + ' damage per turn)'){
-          // calc(player.attack, mTest.attack, player.health, mTest.health)
-          calc(player.attack, monsters.mDummy.attack, player.health, monsters.mDummy.health)
-      }else if(answer === 'run away'){
-        console.log(chalk.magenta('He\'s a dummy. You can attack him once and he will die.'))
-        setTimeout(function(){
-        mDummy()
-        }, 5000);
-      }else{
-        console.log(chalk.magenta('The dummy is too powerful, you can\'t outdodge his greatness.'))
-        setTimeout(function(){
-        firstMonster()
-        }, 5000);
-      }
-  });
 }
 
 function firstMonster(){
@@ -209,6 +189,11 @@ var name =  {
               return console.log("Error writing file: " + err);
           }
       })
+    fs.writeFile(__dirname + "/xp.js", "var xpe = new function(){\n  this.xp = 2.74\n}\n\nexports.xpe = xpe", function (err) {
+          if (err) {
+              return console.log("Error writing file: " + err);
+          }
+      })
       player.username.push(answers.name)
       setTimeout(function(){
         intro()
@@ -226,6 +211,11 @@ var name =  {
 
   inquirer.prompt(name).then(function (answers) {
     fs.writeFile(__dirname + "/username.js", "var username2 = new function(){\n  this.username = \"" + answers.name + "\"\n}\n\nexports.username2 = username2", function (err) {
+          if (err) {
+              return console.log("Error writing file: " + err);
+          }
+      })
+    fs.writeFile(__dirname + "/xp.js", "var xpe = new function(){\n  this.xp = 2.74\n}\n\nexports.xpe = xpe", function (err) {
           if (err) {
               return console.log("Error writing file: " + err);
           }
